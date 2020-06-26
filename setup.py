e52b841b60b63c46b7ad416ea1cab4d882989d60
Phil Barker <phil.barker@pjjk.co.uk>
2020-06-26 16:04:11 +0100

    more setup

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name="CredRegParser", packages=find_packages())
+setup(
+    name="CredRegParser",
+    version="0.1.0",
+    author="Phil Barker",
+    url="https://github.com/philbarker/CredRegParser",
+    description="Fetch and parse resource metadata from the Credential Registry.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages()
+)
